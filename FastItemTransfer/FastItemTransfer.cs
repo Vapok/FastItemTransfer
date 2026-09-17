b1e486bdfa23a4ebb7346623abed4877d00ff3c1
Vapok <vapokrocks@gmail.com>
2026-09-17 05:06:21 -0400

    Release v2.0.5: Jewelcrafting Font Compatibility

--- FastItemTransfer/FastItemTransfer.cs
+++ FastItemTransfer/FastItemTransfer.cs
@@ -23,7 +23,7 @@ namespace FastItemTransfer
         //Module Constants
         private const string _pluginId = "vapok.mods.fastitemtransfer";
         private const string _displayName = "Fast Item Transfer";
-        private const string _version = "2.0.4";
+        private const string _version = "2.0.5";
         
         //Interface Properties
         public string PluginId => _pluginId;
