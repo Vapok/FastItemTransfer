4d6dedaec666a6c09f83f58dfbae578b93454514
Vapok <vapokrocks@gmail.com>
2026-09-18 06:32:41 -0400

    Release v2.0.6: Splash Window Updates & Valheim 1.0.14 Alignment

--- FastItemTransfer/FastItemTransfer.cs
+++ FastItemTransfer/FastItemTransfer.cs
@@ -23,7 +23,7 @@ namespace FastItemTransfer
         //Module Constants
         private const string _pluginId = "vapok.mods.fastitemtransfer";
         private const string _displayName = "Fast Item Transfer";
-        private const string _version = "2.0.5";
+        private const string _version = "2.0.6";
         
         //Interface Properties
         public string PluginId => _pluginId;
